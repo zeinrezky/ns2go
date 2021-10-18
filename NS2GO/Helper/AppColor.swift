331d7c43b1dfd833c86898881729ed39786eec78
Yosua Hoo <yosua.ekowidjaja@dana.id>
2021-10-18 10:12:07 +0700

    add app color constant and mapping alert to enum

---
+++ NS2GO/Helper/AppColor.swift
@@ -0,0 +1,15 @@
+//
+//  AppColor.swift
+//  NS2GO
+//
+//  Created by Yosua Antonio Raphael Ekowidjaja on 18/10/21.
+//
+
+import Foundation
+import UIKit
+
+class AppColor: UIColor {
+	static let indicatorGreen = UIColor(red: 60.0/255.0, green: 244.0/255.0, blue: 110.0/255.0, alpha: 1)
+	static let indicatorYellow = UIColor(red: 250.0/255.0, green: 244.0/255.0, blue: 104.0/255.0, alpha: 1)
+	static let indicatorRed = UIColor(red: 253.0/255.0, green: 165.0/255.0, blue: 154.0/255.0, alpha: 1)
+}
--- NS2GO/Model/AlertLimit.swift
+++ NS2GO/Model/AlertLimit.swift
@@ -9,17 +9,56 @@ import Foundation
 import SwiftyJSON
 
 class AlertLimit {
-	let object: String
-	let entity: String
+	let object: ObjectType
+	let entity: EntityType
 	let warning: String
 	let critical: String
 	let monitor: String
 	
 	init(json: JSON) {
-		object = json["object"].stringValue
-		entity = json["entity"].stringValue
-		warning = json["warning"].stringValue
-		critical = json["critical"].stringValue
-		monitor = json["monitor"].stringValue
+		self.warning = json["warning"].stringValue
+		self.critical = json["critical"].stringValue
+		self.monitor = json["monitor"].stringValue
+	
+		if let objectString = json["object"].string,
+		   let object = ObjectType(rawValue: objectString) {
+			self.object = object
+		} else {
+			self.object = .none
+		}
+		
+		if let entityString = json["entity"].string,
+		   let entity = EntityType(rawValue: entityString) {
+			self.entity = entity
+		} else {
+			self.entity = .none
+		}
+		
+	}
+	
+	enum ObjectType: String {
+		case CLIM = "CLIM"
+		case CPU = "CPU"
+		case Disk = "Disk"
+		case IPU = "IPU"
+		case Process = "Process"
+		case Network = "Network"
+		case TMF = "TMF"
+		case none
+	}
+	
+	enum EntityType: String {
+		case CLIMBusy = "CLIM % Busy"
+		case CLIMMemory = "CLIM Mem Used %"
+		case busy = "% Busy"
+		case lowPinCBS = "% Low PIN PCBs"
+		case memory = "% Memory"
+		case TLE = "% TLE"
+		case queueLength = "Queue Length"
+		case receiveMessage = "Recv Msgs"
+		case sentMessage = "Sent Msgs"
+		case receiveQueue = "Receive Queue"
+		case abort = "% Abort"
+		case none
 	}
 }
